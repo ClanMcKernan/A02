71b03b3f7e42febee110c96561642a5712bdef2a
Zachary <zmckernan1@cougars.ccis.edu>
2021-09-28 16:15:29 -0500

    Initial App.java presented by L from empty A02

---
+++ App.java
@@ -0,0 +1,10 @@
+class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is Assignment 2 for CISS465.");
+		System.out.println("We are using Git for this assignment.");
+		System.out.println("This is the .java file we are using on the repo.");
+		System.out.println("This file needs to be around 10-20 lines");
+	}
+}
