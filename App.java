8d3ed687531fd2bee6526b56834976953e264ea9
Nile <nile.weadick@gmail.com>
2021-09-28 18:15:29 -0500

    changes line 17

--- App.java
+++ App.java
@@ -13,5 +13,7 @@ class App
 		System.out.println("We are using Git for this assignment.");
 		System.out.println("This is the .java file we are using on the repo.");
 		System.out.println("This file needs to be around 10-20 lines");
+
+		//Test if we both push on same version on diff lines
 	}
 }
