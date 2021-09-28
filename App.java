a7169820d5595f718d62fd4ac1ad958a8770614c
Nile <nile.weadick@gmail.com>
2021-09-28 17:36:34 -0500

    Added more output

--- App.java
+++ App.java
@@ -6,5 +6,12 @@ class App
 		System.out.println("We are using Git for this assignment.");
 		System.out.println("This is the .java file we are using on the repo.");
 		System.out.println("This file needs to be around 10-20 lines");
+
+		// Comment made by Nile Weadick1
+		System.out.println("by Nile Weadick");
+		System.out.println("This is Assignment 2 for CISS465.");
+		System.out.println("We are using Git for this assignment.");
+		System.out.println("This is the .java file we are using on the repo.");
+		System.out.println("This file needs to be around 10-20 lines");
 	}
 }
